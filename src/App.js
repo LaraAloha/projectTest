388f5d7b69e1ad985a4555dcdeed51ecb56c603f
Larisa Bulacheva <i_lara94@mail.ru>
2019-07-15 21:18:08 +0300

    added first block of questions and radio buttons

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ import Results from './Results';
 import './App.css';
 import styled from 'styled-components';
 
-
 const Button = styled.button`
 	cursor: pointer;
 	font-size: 1rem;
 	margin: 2%;
 `;
-
 
 /**
  * @todo  В стейт можно прописать номер вопроса, на котором пользователь сидит..
@@ -24,29 +22,16 @@ class App extends React.Component {
 		this.state = {isToggleOn: true};		
 	}
 	render() {
-		if(this.state.isToggleOn) {
 			return (
 				<div className="App">
 					<Questionnaire />
 					<Button> 
 						Узнать результат 
 					</Button>
-					{/* { isToggleOn ? <Results /> : null } */}
-
-					<Results />
+					{ this.state.isToggleOn ? <Results /> : null }
 				</div>
 			);
-		} else {
-			return (
-				<div className="App">
-					<Questionnaire />
-					<Button onClick={this.handleClick}> 
-						Узнать результат
-					</Button>
-					</div>
-			);
-		}
+		} 
 	}
-}
 
 export default App;
