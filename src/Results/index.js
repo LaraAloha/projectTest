943d581e62296f62f8a9cf030bcae26735b0d79c
Larisa Bulacheva <i_lara94@mail.ru>
2019-07-16 10:14:24 +0300

    fixed pdf file creating

--- src/Results/index.js
+++ src/Results/index.js
@@ -1,90 +1,93 @@
 import React from 'react';
+import {ReactDOM} from 'react-dom';
+import ReactDOMServer from "react-dom/server";
+import {render} from 'react-dom';
 import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
 import RadarChart from 'react-svg-radar-chart';
 import 'react-svg-radar-chart/build/css/index.css'
+import styled from 'styled-components';
+import './results.css';
+
+const divWrap = styled.div`
+	width: 30%;
+	
+`;
 
 /**
  * @todo  Результат в ПДФ некорректный -- исправить
  */
 
-function Results({data}) {
+class Results extends React.Component {	
 
-	function countResult(number) {
-		let result = 0;
-		for (let stage = 1; stage <= 4; stage++) {
-			 const q = data.stages[stage].questions[number];
-			 if (q.answer == q.correctAnswer) {
-				   result++;
-			 }
+	render() {
+		const data = this.props.data;
+		
+		const dataResult = [
+			{
+			  data: {
+				sales: countResult(1),
+				planning: countResult(2),
+				voronka: countResult(3),
+				offer: countResult(4),
+				hire: countResult(5),
+				teaching: countResult(6),
+				motivation: countResult(7),
+				leading: countResult(8),
+				scripts: countResult(9),
+				crm: countResult(10),
+				commercial: countResult(11),
+				clients: countResult(12),
+			  },
+			  meta: { color: 'blue' }
+			},
+		  ];
+	   
+	  const captions = {
+			sales: 'Модель продаж',
+			planning: 'Планирование',
+			voronka: 'Воронка',
+			offer: 'Оффер',
+			hire: 'Найм',
+			teaching: 'Обучение и адаптация',
+			motivation: 'Мотивация',
+			leading: 'Управление',
+			scripts: 'Скрипты',
+			crm: 'CRM',
+			commercial: 'Коммерческое предложение',
+			clients: '«Подогрев» клиентов',
+		  };
+
+		function countResult(number) {
+			let result = 0;
+			for (let stage = 1; stage <= 4; stage++) {
+				 const q = data.stages[stage].questions[number];
+				 if (q.answer == q.correctAnswer) {
+					   result++;
+				 }
+			}
+			return result/4;
 		}
-		return result/4;
+
+		return (
+		<divWrap>
+			<div className="wrapper" id="capture">
+				<header>
+					<p>
+						Результаты
+					</p>
+					<RadarChart
+						captions={captions}
+						data={dataResult}
+						size={450}
+					/>
+				</header>
+				<button onClick={window.print}>
+					Скачать результаты в PDF
+				</button>
+			</div>
+		</divWrap>
+		);
 	}
-	
-	function exportPdf() {
-		html2canvas(document.querySelector("#capture")).then(canvas => {
-			document.body.appendChild(canvas);
-			const imgData = canvas.toDataURL('image/png');
-			const pdf = new jsPDF();
-			pdf.addImage(imgData, 'PNG', 0, 0);
-			pdf.save("TestResults.pdf"); 
-		});
-	}
-	
-	const dataResult = [
-		{
-		  data: {
-			sales: countResult(1),
-			planning: countResult(2),
-			// voronka: countResult(3),
-			// offer: countResult(4),
-			// hire: countResult(5),
-			// teaching: countResult(6),
-			// motivation: countResult(7),
-			// leading: countResult(8),
-			// scripts: countResult(9),
-			// crm: countResult(10),
-			// commercial: countResult(11),
-			// clients: countResult(12),
-		  },
-		  meta: { color: 'blue' }
-		},
-	  ];
-   
-  const captions = {
-		// columns
-		sales: 'Модель продаж',
-		planning: 'Планирование',
-		// voronka: 'Воронка',
-		// offer: 'Оффер',
-		// hire: 'Найм',
-		// teaching: 'Обучение и адаптация',
-		// motivation: 'Мотивация',
-		// leading: 'Управление',
-		// scripts: 'Скрипты',
-		// crm: 'CRM',
-		// commercial: 'Коммерческое предложение',
-		// clients: '«Подогрев» клиентов',
-	  };
-	  
-	  return (
-    <div id="capture" className="App">
-      <header className="App-header">
-			<p>
-				Результаты по итогам ответов на вопросы
-			</p>
-			<RadarChart
-				captions={captions}
-				data={dataResult}
-				size={450}
-			/>
-			
-			 <button onClick={exportPdf}>
-				Скачать PDF
-			</button>
-      </header>
-    </div>
-  );
 }
 
 export default Results;
